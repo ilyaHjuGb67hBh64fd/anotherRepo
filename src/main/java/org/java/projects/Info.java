5273ca9dcc4cc9500d9841f12dc0620f1dd4ef88
iluserman <ilyag87@mail.ru>
2022-07-19 22:22:46 +0300

    add change class Users, & adding JavaDoc ot methods

---
+++ src/main/java/org/java/projects/Info.java
@@ -0,0 +1,4 @@
+package org.java.projects;
+
+public class Info {
+}
--- src/test/java/org/java/projects/AppTest.java
+++ src/test/java/org/java/projects/AppTest.java
@@ -1,8 +1,8 @@
 package org.java.projects;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
